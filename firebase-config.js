2e9905e24c6e8551c7163f0f95564bcad022dfbc
poon1000duu-max <poon1000duu@gmail.com>
2026-08-10 21:28:24 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,8 +4,7 @@ export const firebaseConfig = {
   projectId: "order-2b260",
   storageBucket: "order-2b260.firebasestorage.app",
   messagingSenderId: "62062876103",
-  appId: "1:62062876103:web:f7f2dcaacc47d19d7935e2",
-  measurementId: "G-9QHTBWFKGH"
+  appId: "1:62062876103:web:f7f2dcaacc47d19d7935e2"
 };
 
 export const ADMIN_EMAIL = "poon1000duu@gmail.com";
